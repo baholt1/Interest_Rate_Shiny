5b78994df29fee4f939edaaea72e5a5550ec7481
baholt1 <baholt1@ualberta.ca>
2024-02-29 15:22:01 -0700

    Create Navbar Pages.

---
+++ ui.R
@@ -0,0 +1,29 @@
+library(shiny)
+
+navbarPage("Interest Rates",
+  tabPanel("Interest Rates 1",
+    sidebarLayout(
+      sidebarPanel(
+        sliderInput(
+          "rate",
+          "Rates:",
+          min = 0.01,
+          max = 0.10,
+          value = 0.05)
+        ),
+    mainPanel()
+    )
+  ),
+  tabPanel("Interest Rates 2",
+    sidebarLayout(
+      sidebarPanel(
+        selectInput(
+          "userrate",
+          "Choose a Duration:",
+          choices = tickers
+        )
+      ),
+      mainPanel()
+    )
+  )
+)
