0dfcbcf33d5be0ebdc2989fb6f5cfd8ef332f942
simon31fu <102628310+simon31fu@users.noreply.github.com>
2024-03-06 10:21:13 -0700

    reverted global.R

--- global.R
+++ global.R
@@ -23,30 +23,16 @@ rateData <- tidyquant::tq_get(tickers,
   tidyr::drop_na() %>% 
   dplyr::mutate(date,
                 maturity = as.numeric(stringr::str_replace_all(symbol, "(?i)DGS", "")),
-                rate = price / 100, .keep = "none") %>% 
-  dplyr::group_by(maturity) %>% 
-  dplyr::mutate(changeBPS = (rate - dplyr::lag(rate)) * 10000) %>% 
-  tidyr::drop_na() %>% 
-  dplyr::filter(maturity == 1)## trimmed down from full vector of maturities. Remove once c++ function is done
-  
-## final dataframe that results from the FRED dataframe being fed into the bondsuite_calculation C++ function
-# calculatedData <- bondsuite_calculation(x = as.matrix(rateData %>% dplyr::mutate(date = as.numeric(date)))) %>% # Dataframe must be fed into the function as a matrix to comply with C++, does not support Date type meaning that column must be converted to numeric
-#   ## converting back into a dataframe, reverting the Date column back to its proper type, grouping by maturity
-#   dplyr::as_tibble(res) %>%
-#   dplyr::mutate(date = as.Date(date)) %>%
-#   dplyr::group_by(maturity)
-                # rate = price / 100, .keep = "none")
+                rate = price / 100, .keep = "none")
+
 ## regardless of complexity and length, we can add any number of additional columns here in R
 ## as long as they do not call functions (i.e. dplyr::lag)
 ## ensures fastest execution time
-
-
-## data frame must be converted to matrix (Cote does this and directly inserting it is pretty complicated)  
-# calculatedData <- mycppFunction(x = as.matrix(rateData %>% mutate(date = as.numeric(date)))) %>%
-#   ## conversion back to data frame and grouped
-#   dplyr::as_tibble(res) %>%
-#   dplyr::mutate(date = as.Date(date)) %>%
-#   dplyr::group_by(maturity)
+calculatedData <- mycppFunction(x = as.matrix(rateData %>% mutate(date = as.numeric(date)))) %>%
+  ## conversion back to data frame and grouped
+  dplyr::as_tibble(res) %>%
+  dplyr::mutate(date = as.Date(date)) %>%
+  dplyr::group_by(maturity)
 
 
 ## loading a function takes the grand majority of the loading time, meaning ideal app performance is achieved with all calculations done in a single C++ file.
